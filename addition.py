610d463aa9b0142f06442d9f1103022fc2f95abf
Ommarselim <93024155+Ommarselim@users.noreply.github.com>
2021-10-24 01:57:26 +0200

    Add files via upload
    Sheet_1_AI_solutions

---
+++ addition.py
@@ -0,0 +1,29 @@
+# addition.py
+# -----------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+Run python autograder.py
+"""
+
+# this program will add two numbers enterd by the user
+numb1 = input('Enter first number: ')
+numb2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(numb1) + float(numb2)
+
+# Display the sum of the two numbers
+print(f"the sum of {numb1} and {numb2} is equal to {sum}")
+
+
---
+++ shopsmart.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 24 01:48:53 2021
+
+@author: Alahram
+"""
+
+import shop 
+
+def shopSmart(orderList, fruitShops):
+    """
+        orderList: List of (fruit, numPound) tuples
+        fruitShops: List of FruitShops
+    """    
+    
+    minCost, argMin = None, None
+    for shoop in fruitShops:
+        cost = shop.getPriceOfOrder(orderList)
+        if minCost == None or cost < minCost:
+            minCost, argMin = cost, shoop
+    return argMin
+    
+if __name__ == '__main__':
+  orders = [('apples',1.0), ('oranges',3.0)]
+  dir1 = {'apples': 2.0, 'oranges':1.0}
+  shop1 =  shop.FruitShop('shop1',dir1)
+  dir2 = {'apples': 1.0, 'oranges': 5.0}
+  shop2 = shop.FruitShop('shop2',dir2)
+  shops = [shop1, shop2]
+  print ("For orders: ", orders, "best shop is", shopSmart(orders, shops).getName())
+  orders = [('apples',3.0)]
+  print( "For orders: ", orders, "best shop is", shopSmart(orders, shops).getName())
